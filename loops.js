b5cbfdb3717e30b93478be04efd367440be16d15
Maria Dees <kingsgrl2010@yahoo.com>
2018-04-10 20:35:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function doWhileLoop(array) {
    return Math.random() >= 0.5
  }
    do {
-     console.log(--countdown)
+     console.log(array - 1)
    }
    while(array.length > 0 && maybeTrue())
  return array;
